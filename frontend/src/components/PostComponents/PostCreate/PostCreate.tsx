f39c19bab703cd69af048c4efda6733efc6244e6
JunYoung Yeom <yeomjy111@gmail.com>
2022-11-08 12:09:15 +0900

    Post create frontend feature Backend fix

--- frontend/src/components/PostComponents/PostCreate/PostCreate.tsx
+++ frontend/src/components/PostComponents/PostCreate/PostCreate.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -10,9 +11,9 @@ import { AppDispatch } from '../../../store'
 import { createPost, postCreateType } from '../../../store/slices/post'
 import { selectUser } from '../../../store/slices/user'
 import { MdArrowBack } from 'react-icons/md'
-import Combobox from "react-widgets/Combobox";
-import DropdownList from "react-widgets/DropdownList";
-import "react-widgets/scss/styles.scss";
+import Combobox from 'react-widgets/Combobox'
+import DropdownList from 'react-widgets/DropdownList'
+import 'react-widgets/scss/styles.scss'
 import './PostCreate.scss'
 import { Dictionary } from '@reduxjs/toolkit'
 import { List } from 'reselect/es/types'
@@ -36,10 +37,23 @@ export default function PostCreate() {
 	// const vaccination = useRef<HTMLInputElement>(null)
 	// const neutering = useRef<HTMLInputElement>(null)
 	// const character = useRef<HTMLTextAreaElement>(null)
-	const [file, setFile] = useState<{}>({ selectedFiles: null })
+	// const [file, setFile] = useState<{}>({ selectedFiles: null })
+	const [file, setFile] = useState<File[]>([])
 
 	const navigate = useNavigate()
-    const dispatch = useDispatch()
+	const dispatch = useDispatch<AppDispatch>()
+
+	const fields = [
+		title,
+		name,
+		animalType,
+		species,
+		age,
+		gender,
+		vaccination,
+		neutering,
+		character
+	]
 
 	// useEffect(() => {
 	// 	if (!userState.currentUser) navigate("/login");
@@ -47,31 +61,46 @@ export default function PostCreate() {
 
 	const fileChangedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const files = event.target.files
-		console.log(files)
-		setFile({
-			selectedFiles: files
-		})
+		if (files !== null) setFile(file.concat(Array.from(files)))
+		console.log(file)
+		// console.log(files)
+		// setFile({
+		// 	selectedFiles: files
+		// })
 	}
 
-	const createPostHandler = async (
-		event: React.FormEvent<HTMLFormElement>
-	) => {
-		console.log('createPost')
+	const createPostHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		// console.log('createPost')
 		event.preventDefault()
+
+		const emptyField = fields.filter((x) => x === '')
+		if (emptyField.length !== 0) return
+
+		if (file.length === 0) return
+
+		const ageInt = parseInt(age)
+		if (isNaN(ageInt) || ageInt <= 0 || ageInt > 30) return
+
 		const data = {
 			title: title,
 			name: name,
 			animal_type: animalType,
 			species: species,
-			age: age,
-			gender: gender==='수컷' ? true : false,
-			neutering: neutering==='O' ? true : false,
-			vaccination: vaccination==='O' ? true : false,
-			character: character,
-			photo_path: [],
-			author_id: 0,
-		};
-        console.log(data)
+			age: ageInt,
+			gender: gender === '수컷',
+			neutering: neutering === 'O',
+			vaccination: vaccination === 'O',
+			character: character
+			// photo_path: [],
+			// author_id: 0
+		}
+		const formData = new FormData()
+		formData.append('content', JSON.stringify(data))
+		file.forEach((f, i) => formData.append('photos', f))
+
+		dispatch(createPost(formData))
+
+		// console.log(data)
 		// const result = await dispatch(createPost(data));
 		// if (result.type === `${createPost.typePrefix}/fulfilled`) {
 		// 	navigate(`/posts/${result.payload.id}`);
@@ -80,11 +109,32 @@ export default function PostCreate() {
 		// }
 	}
 
-    const speciesList: Dictionary<List> = {
-		"개": ['포메라니안', '치와와', '파피용', '닥스훈트', '요크셔테리어', '말티즈', '슈나우저','시츄', '푸들', '웰시코기'],
-		"고양이":['러시안 블루', '페르시안', '뱅갈', '봄베이', '샴', '메인쿤', '스코티쉬폴드', '아메리칸 숏헤이', '캘리포니아 스팽글드', '이집트안마우']
+	const speciesList: Dictionary<List> = {
+		개: [
+			'포메라니안',
+			'치와와',
+			'파피용',
+			'닥스훈트',
+			'요크셔테리어',
+			'말티즈',
+			'슈나우저',
+			'시츄',
+			'푸들',
+			'웰시코기'
+		],
+		고양이: [
+			'러시안 블루',
+			'페르시안',
+			'뱅갈',
+			'봄베이',
+			'샴',
+			'메인쿤',
+			'스코티쉬폴드',
+			'아메리칸 숏헤이',
+			'캘리포니아 스팽글드',
+			'이집트안마우'
+		]
 	}
-	
 
 	// if (!userState.currentUser) {
 	// 	return <Navigate to="/login" />;
@@ -93,7 +143,7 @@ export default function PostCreate() {
 		<Layout>
 			<div className='CreateContainer'>
 				<div className='PostCreate'>
-					<button	
+					<button
 						id='back-create-post-button'
 						onClick={(event) => {
 							event.preventDefault()
@@ -113,73 +163,90 @@ export default function PostCreate() {
 							<div className='input-container'>
 								<label htmlFor='post-title-input'>제목:</label>
 								<input
-                                    // ref={title}
+									// ref={title}
 									className='post-input'
 									id='post-title-input'
 									type='text'
 									name='title'
-                                    onChange={event => setTitle(event.target.value)}
-                                    value={title}
+									onChange={(event) =>
+										setTitle(event.target.value)
+									}
+									value={title}
 								/>
 							</div>
 							<div className='input-container'>
 								<label htmlFor='post-name-input'>이름:</label>
 								<input
-                                    // ref={name}
+									// ref={name}
 									className='post-input'
 									id='post-name-input'
 									type='text'
 									name='name'
-                                    onChange={event => setName(event.target.value)}
-                                    value={name}
+									onChange={(event) =>
+										setName(event.target.value)
+									}
+									value={name}
 								/>
 							</div>
 							<div className='input-container'>
 								<label htmlFor='post-type-input'>동물:</label>
 								<Combobox
-                                    // ref={animalType}
+									// ref={animalType}
 									id='post-type-input'
 									className='post-combobox'
 									name='type'
-									data={["개", "고양이", "새", "토끼", "거북이", "기타"]}
-                                    onChange={event => setAnimalType(event)}
-                                    value={animalType}
+									data={[
+										'개',
+										'고양이',
+										'새',
+										'토끼',
+										'거북이',
+										'기타'
+									]}
+									onChange={(event) => setAnimalType(event)}
+									value={animalType}
 								/>
 							</div>
 							<div className='input-container'>
 								<label htmlFor='post-species-input'>종:</label>
 								<Combobox
-                                    // ref={species}
+									// ref={species}
 									id='post-species-input'
 									className='post-combobox'
 									name='species'
-									data={speciesList[animalType] ? speciesList[animalType] : []}
-                                    onChange={event => setSpecies(event)}
-                                    value={species}
+									data={
+										speciesList[animalType]
+											? speciesList[animalType]
+											: []
+									}
+									onChange={(event) => setSpecies(event)}
+									value={species}
 								/>
 							</div>
 							<div className='input-container'>
 								<label htmlFor='post-age-input'>나이:</label>
 								<input
-                                    // ref={age}
+									// ref={age}
 									className='post-input'
 									id='post-age-input'
 									type='text'
 									name='age'
-									onChange={event => setAge(event.target.value)}
-                                    value={age}
+									onChange={(event) =>
+										setAge(event.target.value)
+									}
+									value={age}
 								/>
 							</div>
 							<div className='input-container'>
 								<label htmlFor='post-gender-input'>성별:</label>
 								<DropdownList
-                                    // ref={gender}
+									// ref={gender}
 									id='post-gender-input'
 									className='post-dropbox'
 									name='gender'
-									data={["암컷", "수컷"]}
-									onChange={event => setGender(event)}
-                                    value={gender}
+									data={['암컷', '수컷']}
+									onChange={(event) => setGender(event)}
+									value={gender}
 								/>
 							</div>
 							<div className='input-container'>
@@ -187,13 +254,13 @@ export default function PostCreate() {
 									백신 접종 여부:
 								</label>
 								<DropdownList
-                                    // ref={vaccination}
+									// ref={vaccination}
 									id='post-vaccination-input'
 									className='post-dropbox'
 									name='vaccination'
-									data={["O", "X"]}
-									onChange={event => setVaccination(event)}
-                                    value={vaccination}
+									data={['O', 'X']}
+									onChange={(event) => setVaccination(event)}
+									value={vaccination}
 								/>
 							</div>
 							<div className='input-container'>
@@ -201,13 +268,13 @@ export default function PostCreate() {
 									중성화 여부:
 								</label>
 								<DropdownList
-                                    // ref={neutering}
+									// ref={neutering}
 									id='post-neutering-input'
 									className='post-dropbox'
 									name='neutering'
-									data={["O", "X"]}
-									onChange={event => setNeutering(event)}
-                                    value={neutering}
+									data={['O', 'X']}
+									onChange={(event) => setNeutering(event)}
+									value={neutering}
 								/>
 							</div>
 							<div className='content-container'>
@@ -216,12 +283,14 @@ export default function PostCreate() {
 									입양에 도움이 됩니다:&#41;
 								</label>
 								<textarea
-                                    // ref={character}
+									// ref={character}
 									className='post-input'
 									id='post-content-input'
 									name='content'
-									onChange={event => setCharacter(event.target.value)}
-                                    value={character}
+									onChange={(event) =>
+										setCharacter(event.target.value)
+									}
+									value={character}
 								/>
 							</div>
 							<div className='input-container'>
@@ -238,8 +307,8 @@ export default function PostCreate() {
 							<button
 								id='confirm-create-post-button'
 								type='submit'
-								// disabled={!(title && name && animalType && species && age 
-                                //     && gender && vaccination && neutering && character)}
+								// disabled={!(title && name && animalType && species && age
+								//     && gender && vaccination && neutering && character)}
 							>
 								게시하기
 							</button>
