ef9694b65078688303644059d0884fbf2cad93a1
Suebin <seoil5533@naver.com>
2022-11-13 00:44:36 +0900

    Test - ReviewDetail

---
+++ frontend/src/components/ReviewComponents/ReviewDetail/ReviewDetail.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import ReviewDetail from "./ReviewDetail";
+
+describe("<ReviewDetail />", () => {
+    it("should render without errors", () => {
+        render(
+            <ReviewDetail
+                title={"REVIEWDETAIL_TITLE"}
+                photo_path={[]}
+                author={"REVIEWDETAIL_AUTHOR"}
+                content={"REVIEWDETAIL_CONTENT"}
+            />
+        );
+        screen.getByText("REVIEWDETAIL_TITLE");
+        screen.getByText("[]");
+        screen.getByText("REVIEWDETAIL_AUTHOR");
+        screen.getByText("REVIEWDETAIL_CONTENT");
+    });
+});
