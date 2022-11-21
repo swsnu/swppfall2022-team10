97b894f37e6a34d14b4d92828f6f0004ec303f6e
Suebin <seoil5533@naver.com>
2022-11-21 19:08:50 +0900

    Qna-QnaDetail

--- frontend/src/components/QnaComponents/QnaDetail/QnaDetail.tsx
+++ frontend/src/components/QnaComponents/QnaDetail/QnaDetail.tsx
@@ -1,34 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable react/jsx-key */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import Layout from '../../Layout/Layout'
-
-/* import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
+import { selectQna, qnaActions } from '../../../store/slices/qna'
+import './QnaDetail.scss'
+
+
+/* 
 import { useNavigate } from 'react-router-dom'
 import { AppDispatch } from '../../../store'
 import { selectPost, getPost, deletePost } from '../../../store/slices/post'
 import { FaSyringe, FaRegMeh } from 'react-icons/fa'
 import PostHeader from '../PostHeader/PostHeader'
+ */
 
-import './PostDetail.scss'
 
-interface IProps {
-    is_author: boolean
-} */
+const QnaDetail = () => {
+    const { id } = useParams()
+    const dispatch = useDispatch()
+    const qnaState = useSelector(selectQna)
 
-export default function QnaDetail() {
+    useEffect(() => {
+        dispatch(qnaActions.getQna({ targetId: Number(id) }));
+    }, [dispatch, id])
+
     return (
         <Layout>
-            <div className='title'>
-                Qna Detail 들어갈 Page
+            <div className='DetailContainer'>
+                <div className='title'>
+                    {qnaState.selectedQna?.title}
+                </div>
+                <div className='content'>
+                    {qnaState.selectedQna?.content}
+                </div>
             </div>
-
         </Layout>
     )
 }
+
+export default QnaDetail
 
 /* const PostDetail = (props: IProps) => {
     const { id } = useParams()
--- frontend/src/store/slices/qna.ts
+++ frontend/src/store/slices/qna.ts
@@ -4,6 +4,7 @@ import { RootState } from '..'
 export interface QnaType {
     id: number
     title: string
+    content: string
     created_at: string
     hits: number
 }
@@ -15,12 +16,12 @@ export interface qnaState {
 
 const initialState: qnaState = {
     qnas: [
-        { id: 1, title: "Be-a-family", created_at: "2022-11-20", hits: 1 },
-        { id: 2, title: "Be-a-family", created_at: "2022-11-20", hits: 1 },
-        { id: 3, title: "Be-a-family", created_at: "2022-11-20", hits: 1 },
-        { id: 4, title: "working", created_at: "2022-11-11", hits: 3 },
-        { id: 5, title: "eslint sucks", created_at: "2022-11-11", hits: 3 },
-        { id: 6, title: "6th", created_at: "2022-11-11", hits: 4 },
+        { id: 1, title: "Be-a-family", content: "개가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-20", hits: 1 },
+        { id: 2, title: "Be-a-family", content: "고양이가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-20", hits: 1 },
+        { id: 3, title: "Be-a-family", content: "앵무새가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-20", hits: 1 },
+        { id: 4, title: "working", content: "햄스터가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-11", hits: 3 },
+        { id: 5, title: "eslint sucks", content: "친칠라가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-11", hits: 3 },
+        { id: 6, title: "6th", content: "고슴도치가 말을 듣지 않아요. 어떻게 하죠?", created_at: "2022-11-11", hits: 4 },
     ],
     selectedQna: null
 }
@@ -31,15 +32,21 @@ export const qnaSlice = createSlice({
     initialState,
     reducers: {
         getAll: (state, action: PayloadAction<{ qnas: QnaType[] }>) => { },
-        getQna: (state, action: PayloadAction<{ targetId: number }>) => { },
-        deleteQna: (state, action: PayloadAction<{ targetId: number }>) => { },
+        getQna: (state, action: PayloadAction<{ targetId: number }>) => {
+            const target = state.qnas.find((td) => td.id === action.payload.targetId)
+            state.selectedQna = target ?? null
+        },
+        deleteQna: (state, action: PayloadAction<{ targetId: number }>) => {
+
+        },
         addQna: (
             state,
-            action: PayloadAction<{ title: string }>
+            action: PayloadAction<{ title: string, content: string }>
         ) => {
             const newQna = {
                 id: state.qnas[state.qnas.length - 1].id + 1,
                 title: action.payload.title,
+                content: action.payload.content,
                 created_at: '2022-11-20',
                 hits: 1,
             };
